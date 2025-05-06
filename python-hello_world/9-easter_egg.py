6751992464205ceb3908e2ffd4802bff71ffdb30
MinutePapillon <christophebarrere@gmail.com>
2025-05-06 11:15:06 +0200

    try to reduce the number of character

--- python-hello_world/9-easter_egg.py
+++ python-hello_world/9-easter_egg.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-str = """The Zen of Python, by Tim Peters
+a = " is better than "
+b = "If the implementation is "
+print(f"""The Zen of Python, by Tim Peters
 
-Beautiful is better than ugly.
-Explicit is better than implicit.
-Simple is better than complex.
-Complex is better than complicated.
-Flat is better than nested.
-Sparse is better than dense.
+Beautiful{a}ugly.
+Explicit{a}implicit.
+Simple{a}complex.
+Complex{a}complicated.
+Flat{a}nested.
+Sparse{a}dense.
 Readability counts.
 Special cases aren't special enough to break the rules.
 Although practicality beats purity.
@@ -15,9 +17,8 @@ Unless explicitly silenced.
 In the face of ambiguity, refuse the temptation to guess.
 There should be one-- and preferably only one --obvious way to do it.
 Although that way may not be obvious at first unless you're Dutch.
-Now is better than never.
+Now{a}never.
 Although never is often better than *right* now.
-If the implementation is hard to explain, it's a bad idea.
-If the implementation is easy to explain, it may be a good idea.
-Namespaces are one honking great idea -- let's do more of those!"""
-print(str)
+{b}hard to explain, it's a bad idea.
+{b}easy to explain, it may be a good idea.
+Namespaces are one honking great idea -- let's do more of those!""")
