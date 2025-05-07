2acf0e256456aff49b2dfdb7562dffec9abc3724
MinutePapillon <christophebarrere@gmail.com>
2025-05-07 09:04:53 +0200

    fix: import this

--- python-hello_world/9-easter_egg.py
+++ python-hello_world/9-easter_egg.py
@@ -1,24 +1,2 @@
 #!/usr/bin/python3
-a = " is better than "
-b = "If the implementation is "
-print(f"""The Zen of Python, by Tim Peters
-
-Beautiful{a}ugly.
-Explicit{a}implicit.
-Simple{a}complex.
-Complex{a}complicated.
-Flat{a}nested.
-Sparse{a}dense.
-Readability counts.
-Special cases aren't special enough to break the rules.
-Although practicality beats purity.
-Errors should never pass silently.
-Unless explicitly silenced.
-In the face of ambiguity, refuse the temptation to guess.
-There should be one-- and preferably only one --obvious way to do it.
-Although that way may not be obvious at first unless you're Dutch.
-Now{a}never.
-Although never is often better than *right* now.
-{b}hard to explain, it's a bad idea.
-{b}easy to explain, it may be a good idea.
-Namespaces are one honking great idea -- let's do more of those!""")
+import this
--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -3,4 +3,4 @@
 for i in range(1, 100):
     print("0{}".format(i) if i < 10 else "{}".format(i), end="" if i < 99 else "\n")
     if i < 99:
-        print(", ".format(i), end="")
+        print(", ", end="")
