3f91b114dc5e91204a185ca775587cf85c9706ac
MinutePapillon <christophebarrere@gmail.com>
2025-05-07 09:43:18 +0200

    fix: use argument in {}

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 
 for i in range(1, 100):
-    print("0{}".format(i) if i < 10 else "{}".format(i), end="" if i < 99 else "\n")
-    if i < 99:
-        print(", ", end="")
+    print("{:02d}".format(i), end=", " if i < 99 else "\n")
