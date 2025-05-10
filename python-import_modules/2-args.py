b80c9b68343a1b1bcf52f12b66c824f7190acff5
MinutePapillon <christophebarrere@gmail.com>
2025-05-10 13:53:58 +0200

    Add a python script that return the number of arguments and do the list

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+i = 1
+length = len(sys.argv) - 1
+
+if length == 0:
+    print("0 arguments.")
+elif length == 1:
+    print("1 argument:")
+else:
+    print("{} arguments:".format(length))
+
+while i < length + 1:
+    print("{}: {}".format(i, sys.argv[i]))
+    i += 1
