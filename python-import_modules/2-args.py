eae3c9d1073c0bae007c72daaed097fe6eb5757a
MinutePapillon <christophebarrere@gmail.com>
2025-05-10 13:56:35 +0200

    fix: add if __name__ == "__main__":

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-import sys
-i = 1
-length = len(sys.argv) - 1
+if __name__ == "__main__":
+    import sys
+    i = 1
+    length = len(sys.argv) - 1
 
-if length == 0:
-    print("0 arguments.")
-elif length == 1:
-    print("1 argument:")
-else:
-    print("{} arguments:".format(length))
+    if length == 0:
+        print("0 arguments.")
+    elif length == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(length))
 
-while i < length + 1:
-    print("{}: {}".format(i, sys.argv[i]))
-    i += 1
+    while i < length + 1:
+        print("{}: {}".format(i, sys.argv[i]))
+        i += 1
