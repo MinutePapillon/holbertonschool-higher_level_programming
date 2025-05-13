7745bfbb5c075cf1b68744b805072cf162f3a38a
MinutePapillon <christophebarrere@gmail.com>
2025-05-13 14:10:35 +0200

    fix: add a condition if the sentence is empty

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    tuple = (len(sentence), sentence[0])
-    return tuple
+    if sentence == "":
+        return (0, None)
+    return (len(sentence), sentence[0])
