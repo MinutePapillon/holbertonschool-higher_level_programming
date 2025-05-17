45174c4274780921e8586ad18ec71bfe39b41c73
MinutePapillon <christophebarrere@gmail.com>
2025-05-17 15:21:34 +0200

    fix: add a condition if roman letter is sup or inf at num

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    roman_numeral = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    if roman_string is None:
+        return 0
+    elif not isinstance(roman_string, str):
+        return 0
+
     result = 0
+    num = 0
+    roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     for letter in roman_string:
-        for number in roman_numeral.keys():
-            if letter == number:
-                result += roman_numeral[number]
+        if roman[letter] > num:
+            result -= num
+            num = roman[letter] - num
+        else:
+            num = roman[letter]
+        result += num
+
     return result
