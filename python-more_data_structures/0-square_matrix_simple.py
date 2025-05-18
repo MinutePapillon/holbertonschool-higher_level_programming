ea852f0713deaf5b9c2af75e1596fcf08e3b0834
MinutePapillon <christophebarrere@gmail.com>
2025-05-18 11:39:19 +0200

    fix: change the code

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
+
 def square_matrix_simple(matrix=[]):
-    new_matrix = []
-    for row in matrix:
-        new_row = []
-        for element in row:
-            new_row.append(element ** 2)
-        new_matrix.append(new_row)
-    return new_matrix
+    new = matrix.copy()
+    for i in range(len(new)):
+        new[i] = list(map(lambda x: x * x, new[i]))
+    return (new)
