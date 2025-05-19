950b854dc176ff4005c58d1b2be316cf8a042a40
MinutePapillon <christophebarrere@gmail.com>
2025-05-19 19:08:19 +0200

    fix: add docstrings for module and classe

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""
+This module defines a class named Square.
+It is an empty class that serves as a placeholder
+for future development.
+"""
 class Square:
-    """A class that defines a square."""
+    """
+    A class that defines a square.
+    """
     pass
