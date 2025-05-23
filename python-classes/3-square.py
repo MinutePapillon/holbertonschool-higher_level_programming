9946b4a89456e644b2f7a38625f838566d9edffc
MinutePapillon <christophebarrere@gmail.com>
2025-05-23 13:51:26 +0200

    fix: improve Square class docstrings and validate size input

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 0-square.py
-Module containing class Square.
+Module containing the Square class.
 """
 
 
@@ -9,28 +9,27 @@ class Square:
     """
     A class to define a square.
 
-    Private Attributes:
+    Attributes:
         __size (int): The size of the square's side.
     """
+
     def __init__(self, size=0):
         """
-        Initializes a new instance of the Square class.
+        Initializes a new Square instance.
 
         Args:
             size (int): The size of the square's side.
 
-        Private Attributes:
-            __size (int):  The size of the square's side.
-
-        Raise:
-            TypeError: If size is not an integer
-            ValueError: If size is inf to 0
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     def area(self):
         """
         Calculates the area of the square.
