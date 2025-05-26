2670292acf9ba98a1ddf9cd85cb9126d4ae30c23
MinutePapillon <christophebarrere@gmail.com>
2025-05-26 12:56:52 +0200

    Add a function that returns the list of available attributes and methods

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Module that defines a function
+"""
+
+
+def lookup(obj):
+    """
+    Function that returns the list of available 
+    attributes and methods of an object
+    """
+    
+    return dir(obj)
