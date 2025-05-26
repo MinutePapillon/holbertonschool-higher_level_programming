40aa333f26a88870aff6436ce658af8595529194
MinutePapillon <christophebarrere@gmail.com>
2025-05-26 16:10:58 +0200

    Add a fonction that return True if an object is an instance

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module that defines a function to check object inheritance.
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Return True if the object is an instance of, or if
+    the object is an instance of a class that inherited from.
+    
+    Parameters:
+        obj: any object to check
+        a_class: the class to compare against
+        
+    Returns:
+        True if obj is an instance
+        """
+    return isinstance(obj, a_class)
