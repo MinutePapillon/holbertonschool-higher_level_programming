be01c7ee5621d136d7cf9f573df6bab494a42cac
MinutePapillon <christophebarrere@gmail.com>
2025-05-26 16:13:25 +0200

    fix: delete withespace

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -8,11 +8,11 @@ def is_kind_of_class(obj, a_class):
     """
     Return True if the object is an instance of, or if
     the object is an instance of a class that inherited from.
-    
+
     Parameters:
         obj: any object to check
         a_class: the class to compare against
-        
+
     Returns:
         True if obj is an instance
         """
