1cb413e13c94d4d973d6cbfda2bb9fbd865266ab
MinutePapillon <christophebarrere@gmail.com>
2025-05-28 14:24:48 +0200

    Add a method that return the rectangle's area

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Module that defines the BaseGeometry class.
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    Rectangle that inherits from BaseGeometry.
+    """
+    def __init__(self, width, height):
+        """
+        Initializes a rectangle with validated width and height.
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+    def area(self):
+        return self.__width * self.__height
