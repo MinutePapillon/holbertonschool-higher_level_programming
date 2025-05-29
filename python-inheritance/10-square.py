0e8f1f5e007abe420fec813fc87f7c196f2353f6
MinutePapillon <christophebarrere@gmail.com>
2025-05-29 14:54:32 +0200

    Add a Square class that returns the area's square

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Module that defines the Square class
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Square that inherits from Rectangle.
+    """
+
+    def __init__(self, size):
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        return self.__size ** 2
