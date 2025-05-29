4e4a66094a36a67b4f25f95fa9ed6c2e8142e1a1
MinutePapillon <christophebarrere@gmail.com>
2025-05-29 16:48:59 +0200

    fix: use isinstance and type function

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -9,4 +9,4 @@ def inherits_from(obj, a_class):
     Return True if the objects is an instance of a class
     that inherited from the specified class.
     """
-    return type(obj) is not a_class
+    return isinstance(obj, a_class) and type(obj) is not a_class
