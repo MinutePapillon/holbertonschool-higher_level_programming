ad10855edd1a54f252dacab756c2509e225b66cb
MinutePapillon <christophebarrere@gmail.com>
2025-06-02 16:38:52 +0200

    feat: implement the from_json_string function

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module that defines the from_json_string function"""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """
+    Returns an object represented by a JSON string.
+    
+    Args:
+        my_str (JSON string): The JSON string.
+
+    Returns:
+        str: The object represented by a JSON string.
+    """
+    return json.loads(my_str)
