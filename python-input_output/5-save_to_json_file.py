a041152eeb2eac642281757e8388378e88e87e18
MinutePapillon <christophebarrere@gmail.com>
2025-06-02 17:15:05 +0200

    fix: add a docstring for the function

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -5,5 +5,13 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
+    """
+    Writes an object to a text file, using a JS0N representation.
+
+    Args:
+        my_obj: The object to srialize and write.
+        filename (string): The name of the file where write the text.
+
+    """
     with open(filename, "w", encoding="utf-8") as f:
-        return json.dump(my_obj, f)
+        json.dump(my_obj, f)
