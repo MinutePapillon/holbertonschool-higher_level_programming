4e4ee276e0d52ae921f2a49908557ad3208b573c
MinutePapillon <christophebarrere@gmail.com>
2025-06-02 17:55:21 +0200

    feat: implement the load_from_json_file function

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module that defines the load_from_json function."""
+
+import json
+
+
+def load_from_json_file(filename):
+    """
+    Creates an object from a JSON file.
+
+    Args:
+        filename (str): The name of file.
+
+    """
+    with open(filename, "r", encoding="utf-8") as f:
+        json.load(f)
