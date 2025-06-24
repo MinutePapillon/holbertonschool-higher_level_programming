d6e5a8268c89a992b43cdecd9dc45ef740e6ae99
MinutePapillon <christophebarrere@gmail.com>
2025-06-24 22:51:55 +0200

    feat: script that lists all records in a table

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- script that lists all records in a table
-SELECT score, name FROM second_table ORDER BY score DESC LIMIT 2;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
