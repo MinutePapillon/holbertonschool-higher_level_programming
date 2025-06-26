0a0792551d649a40f2ef65e19e79e5e1b1475805
MinutePapillon <christophebarrere@gmail.com>
2025-06-26 14:52:44 +0200

    feat: script that creates a database

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- script that creates a database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name VARCHAR(256)
+);
