18a4ffacc26362ffcf7dff33dccafb967d39277a
MinutePapillon <christophebarrere@gmail.com>
2025-06-27 16:10:39 +0200

    fix

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,29 +1,19 @@
 #!/usr/bin/python3
 """
-Lists all states with a name starting with N (uppercase N)
-from the database `hbtn_0e_0_usa`.
+Lists all states with a name starting with 'N' from the database.
 """
 
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    # Connexion à la base de données
     db = MySQLdb.connect(
-        host="localhost", port=3306,
-        user=argv[1], passwd=argv[2], db=argv[3]
+        host="localhost", user=argv[1], passwd=argv[2], db=argv[3], port=3306
     )
-
-    # Création du curseur
     cur = db.cursor()
-
-    # Exécution de la requête SQL
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-
-    # Récupération et affichage des résultats
-    for row in cur.fetchall():
+    cur.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC", ("N%",))
+    rows = cur.fetchall()
+    for row in rows:
         print(row)
-
-    # Fermeture des connexions
     cur.close()
     db.close()
