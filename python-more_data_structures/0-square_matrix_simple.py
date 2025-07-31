567160bdbea39854139578f0228e7d5a05c47bba
MinutePapillon <159028726+MinutePapillon@users.noreply.github.com>
2025-07-31 14:26:25 +0200

    Update 0-square_matrix_simple.py

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,1 @@
-#!/usr/bin/python3
 
-def square_matrix_simple(matrix=[]):
-    new = matrix.copy()
-    for i in range(len(new)):
-        new[i] = list(map(lambda x: x * x, new[i]))
-    return (new)
