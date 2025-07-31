ef171144dc95338680bac6b7e15064552620dacd
MinutePapillon <159028726+MinutePapillon@users.noreply.github.com>
2025-07-31 11:27:26 +0200

    Update 6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,1 @@
-#!/usr/bin/python3
-for i in range(0, 9):
-    for j in range(1 + i, 10):
-        print("{}{}".format(i, j), end="")
-        if i == 8 and j == 9:
-            print("")
-        else:
-            print(", ", end=", ")
+
