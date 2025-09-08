160999a2e21342eaf6442f1bbe3e58cd790601c3
MinutePapillon <christophebarrere@gmail.com>
2025-09-08 22:31:41 +0200

    fix: add a condition

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -3,7 +3,9 @@ def new_in_list(my_list, idx, element):
     new_list = []
     for i in my_list:
         new_list.append(i)
-    if idx < 0 or idx > len(my_list):
+    if idx < 0:
+        return new_list
+    elif idx > len(my_list):
         return new_list
     else:
         new_list[idx] = element
