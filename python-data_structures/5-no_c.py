64d1cb5c366f1f56cee49cff9347b3ce080e6089
MinutePapillon <christophebarrere@gmail.com>
2025-09-08 22:35:45 +0200

    fix: use list()

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    list_cast = list(my_string)
     new_list = []
-    for i in list_cast:
+    for i in list(my_string):
         if i != 'c' and i != 'C':
             new_list.append(i)
-    #sep = ""
     return "".join(new_list)
