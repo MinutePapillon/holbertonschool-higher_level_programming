3307725c841f8bb0352a082284f101c8d98bc13f
MinutePapillon <christophebarrere@gmail.com>
2025-09-10 18:50:50 +0200

    feat: function that returns the number of keys in a dictionary

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_list = []
+    for x, in enumerate(matrix):
+        new_list[x]
+        for y, j in enumerate(i):
+            new_list[x][y] = j
+    print(new_list)
