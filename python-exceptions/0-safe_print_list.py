b007937c00bc0c692d35fa80c8a8814f37c57c2c
MinutePapillon <christophebarrere@gmail.com>
2025-09-12 14:10:27 +0200

    feat: function that prints x elements of a list

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    for i in my_list:
+        count += 1
+    try:
+        if x > count:
+            raise ValueError
+        j = 0
+        while j != x:
+            print("{}".format(my_list[j]), end="")
+            j += 1
+        print()
+        return x
+    except ValueError:
+        j = 0
+        while j != count:
+            print("{}".format(my_list[j]), end="")
+            j += 1
+        print()
+        return count
