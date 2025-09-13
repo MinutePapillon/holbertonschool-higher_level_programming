881dce3e7a590529a0d6561ef98dd469d1630437
MinutePapillon <christophebarrere@gmail.com>
2025-09-13 13:49:19 +0200

    feat: function that adds 2 integers

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""
+0-add_integer.py
+Module containing a function adding two integers.
+Returns an integer.
+"""
+
+
+def add_integer(a, b=98):
+    """
+    Adds two integers.
+    """
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
+
+    return int(a + b)
