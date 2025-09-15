83fa07da2c345c8e1d20d7e5f44a3493e60e7923
MinutePapillon <christophebarrere@gmail.com>
2025-09-15 13:47:43 +0200

    feat: class Square that defines a square by: (based on 1-square.py)

---
+++ python-classes/2-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+2-square.py
+Module containing class Square.
+"""
+
+
+class Square:
+    """
+    A class to define a square.
+    """
+    def __init__(self, size=0):
+        """Initialize a new instance of Class Square.
+
+        Args:
+            size (int): size of square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
