4117660d70fbcd1ab7418df58084be74bf51f075
MinutePapillon <christophebarrere@gmail.com>
2025-09-22 13:56:36 +0200

    feat: class BaseGeometry (based on 5-base_geometry.py)

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+5-base_geometry.py
+Module that defines a class BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """
+    Empty class.
+    """
+    def area(self):
+        """
+        Function that raises.
+
+        Args:
+            self (): the instance.
+
+        Raises:
+            raises an Exception with the message area() is not implemented.
+
+        Returns:
+            Nothing.
+        """
+        raise Exception("area() is not implemented")
