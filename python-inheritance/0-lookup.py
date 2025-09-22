fefba37d5a42a3e63ab9adae07bd166154ef67ad
MinutePapillon <christophebarrere@gmail.com>
2025-09-22 09:44:23 +0200

    fix: add a blank line

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""
+0-lookup.py
+Module that defines a function lookup.
+"""
+
+
 def lookup(obj):
-    """Function that returns the list of available attributes and methods of an object."""
+    """Function that returns the list of available
+    attributes and methods of an object."""
     return dir(obj)
