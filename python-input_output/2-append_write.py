fff802fcd9526023f1a0562d4ff0ce48fca07511
MinutePapillon <christophebarrere@gmail.com>
2025-09-29 13:36:32 +0200

    fix: delete the function len

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -10,5 +10,4 @@ def append_write(filename="", text=""):
     Function that appends a string at the end of a text file.
     """
     with open(filename, 'a', encoding="utf-8") as f:
-        f.write(text)
-    return len(text)
+        return f.write(text)
