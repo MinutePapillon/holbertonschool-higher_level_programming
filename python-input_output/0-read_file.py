0ceab117d6204628fceebe0535012ad0005885fb
MinutePapillon <christophebarrere@gmail.com>
2025-09-29 11:39:07 +0200

    fix: add docstrings

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+0-read_file.py
+Module containing function to read a file.
+"""
+
+
 def read_file(filename=""):
     """Reads a UTF-8 text file and prints its content to stdout."""
     with open(filename, encoding="utf-8") as f:
